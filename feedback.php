61c9be834e0896b16502fcee3eab79633b6d3996
Vidhish Trivedi <Vidhish.Trivedi@iiitb.ac.in>
2023-03-30 16:06:14 +0530

    Storing data from form in MySQL DB

--- feedback.php
+++ feedback.php
@@ -1,32 +1,10 @@
 <?php include "include/header.php"; ?>
 
 <?php
-    // $feedback_items = [
-    //     [
-    //         "id" => '1',
-    //         "name" => "User 1",
-    //         "email" => "user1@email1",
-    //         "body" => "lorem ipsum from user 1"
-    //     ],
-    //     [
-    //         "id" => '1',
-    //         "name" => "User 2",
-    //         "email" => "user2@email2",
-    //         "body" => "lorem ipsum from user 2"
-    //     ],
-    //     [
-    //         "id" => '3',
-    //         "name" => "User 3",
-    //         "email" => "user3@email3",
-    //         "body" => "lorem ipsum from user 3"
-    //     ],
-    // ];
-
     $sqlQuery = 'select * from feedback';
     $resultSet = mysqli_query($connection, $sqlQuery);
 
     $feedback_items = mysqli_fetch_all($resultSet, MYSQLI_ASSOC);      // Second argument is mode, in this case, an associative array.
-
 ?>
 
     <h2>Past Feedback</h2>
--- index.php
+++ index.php
@@ -1,20 +1,71 @@
 <?php include "include/header.php"; ?>
-<!-- <?php // require "include/header.php"; ?> -->             <!-- require_once / requireonce needs the file to exist! -->
+
+<?php
+    $name = $email = $body = "";
+    $nameError = $emailError = $bodyError = "";
+
+    // Form Validation.
+    if(isset($_POST["submit"])) {
+        if(empty($_POST["name"])) {
+            $nameError = "Name is required";
+        }
+        else{
+            $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        }
+
+        if(empty($_POST["email"])) {
+            $emailError = "Email is required";
+        }
+        else{
+            $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+        }
+
+        if(empty($_POST["body"])) {
+            $bodyError = "Feedback is required";
+        }
+        else{
+            $body = filter_input(INPUT_POST, "body", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        }
+
+        if(empty($nameError) && empty($emailError) && empty($bodyError)) {
+            // Insert to MySQL DB.
+            $date = date("y-m-d");
+            $sqlQuery = "insert into feedback (name, email, body, date) values ('$name', '$email', '$body', '$date')";
+
+            if(mysqli_query($connection, $sqlQuery)) {
+                // On success of query.
+                header("Location: feedback.php");
+            }
+            else {
+                echo "Error: " . mysqli_error($connection);
+            }
+        }
+    }
+?>
 
     <h2>Feedback</h2>
     <p class="lead text-center">Leave your feedback here</p>
-    <form action="" class="mt-4 w-75">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST" class="mt-4 w-75">
       <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+        <input type="text" class="form-control <?php echo $nameError ? 'is-invalid' : null ?>" id="name" name="name" placeholder="Enter your name">
+        <div class="invalid-feedback">
+            <?php echo $nameError ?>
+        </div>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+        <input type="email" class="form-control <?php echo $emailError ? 'is-invalid' : null ?>" id="email" name="email" placeholder="Enter your email">
+        <div class="invalid-feedback">
+            <?php echo $emailError ?>
+        </div>
       </div>
       <div class="mb-3">
         <label for="body" class="form-label">Feedback</label>
-        <textarea class="form-control" id="body" name="body" placeholder="Enter your feedback"></textarea>
+        <textarea class="form-control <?php echo $bodyError ? 'is-invalid' : null ?>" id="body" name="body" placeholder="Enter your feedback"></textarea>
+        <div class="invalid-feedback">
+            <?php echo $bodyError ?>
+        </div>
       </div>
       <div class="mb-3">
         <input type="submit" name="submit" value="Send" class="btn btn-dark w-100">
